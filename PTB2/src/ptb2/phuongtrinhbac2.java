6235e61e9c767f2df7c1dd7bd54ceeb434500089
tanducc111 <tannducc@gmail.com>
2024-04-13 21:16:56 +0700

    Add new

---
+++ PTB2/src/ptb2/phuongtrinhbac2.java
@@ -0,0 +1,35 @@
+package ptb2;
+
+import java.util.Scanner;
+
+public class phuongtrinhbac2 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+
+        System.out.println("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+
+        double delta = b * b - 4 * a * c;
+
+        if (delta > 0) {
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Phương trình có 2 nghiệm phân biệt:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
+            System.out.println("Phương trình có nghiệm kép:");
+            System.out.println("x = " + x);
+        } else {
+            System.out.println("Phương trình vô nghiệm");
+        }
+        scanner.close();
+    }
+}
